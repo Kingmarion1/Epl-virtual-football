b05d01e3f74e3ef69038d5eb43e7f60d4c22c044
Prominent <119831389+Kingmarion1@users.noreply.github.com>
2026-03-12 17:03:02 +0100

    Create server.js

---
+++ backend/src/server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+/* ---------------- MIDDLEWARE ---------------- */
+
+app.use(cors());
+app.use(express.json());
+
+/* ---------------- DATABASE ---------------- */
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB Connected");
+})
+.catch((err) => {
+  console.log("MongoDB Error:", err);
+});
+
+/* ---------------- TEST ROUTE ---------------- */
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "EPL Virtual football API is running ⚽"
+  });
+});
+
+/* ---------------- PORT ---------------- */
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
