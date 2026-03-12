5f84fec3c272127fe153823027fcf453d225546a
Prominent <119831389+Kingmarion1@users.noreply.github.com>
2026-03-12 22:31:02 +0100

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,3 +35,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const startVirtualEngine = require("./engine/virtualEngine");
+
+startVirtualEngine();
