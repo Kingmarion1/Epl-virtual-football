ac5de3ce140925cba581d27184301f8eceeb0e9b
Prominent <119831389+Kingmarion1@users.noreply.github.com>
2026-03-12 23:29:48 +0100

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Matches from "./pages/Matches";
+import Standings from "./pages/Standings";
+import Leaderboard from "./pages/Leaderboard";
+import Profile from "./pages/Profile";
+
+import Navbar from "./components/Navbar";
+
+function App() {
+
+  return (
+
+    <BrowserRouter>
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<Matches />} />
+        <Route path="/matches" element={<Matches />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/table" element={<Standings />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/profile" element={<Profile />} />
+
+      </Routes>
+
+    </BrowserRouter>
+
+  );
+
+}
+
+export default App;
