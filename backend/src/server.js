c7b8fd3ab454da6f01c5d31d52d4a4d5584e578f
Prominent <119831389+Kingmarion1@users.noreply.github.com>
2026-03-12 23:08:43 +0100

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,49 +1,45 @@
 require("dotenv").config();
+
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 
-const app = express();
-
-/* ---------------- MIDDLEWARE ---------------- */
-
-app.use(cors());
-app.use(express.json());
-
-/* ---------------- DATABASE ---------------- */
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("MongoDB Connected");
-})
-.catch((err) => {
-  console.log("MongoDB Error:", err);
-});
-
-/* ---------------- TEST ROUTE ---------------- */
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "EPL Virtual football API is running ⚽"
-  });
-});
-
-/* ---------------- PORT ---------------- */
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-const startVirtualEngine = require("./engine/virtualEngine");
-
-startVirtualEngine();
+const connectDB = require("./config/database");
 
 const matchRoutes = require("./routes/matchRoutes");
 const betRoutes = require("./routes/betRoutes");
 const tableRoutes = require("./routes/tableRoutes");
+const authRoutes = require("./routes/authRoutes");
 
+const startVirtualEngine = require("./engine/virtualEngine");
+
+const app = express();
+
+/* MIDDLEWARE */
+
+app.use(cors());
+app.use(express.json());
+
+/* DATABASE */
+
+connectDB();
+
+/* ROUTES */
+
+app.use("/api/auth", authRoutes);
 app.use("/api/matches", matchRoutes);
 app.use("/api/bets", betRoutes);
 app.use("/api", tableRoutes);
+
+/* START ENGINE */
+
+startVirtualEngine();
+
+/* SERVER */
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+  console.log(`Server running on port ${PORT}`);
+
+});
