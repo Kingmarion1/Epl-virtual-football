4593f7a0a22f54d70e9323a9d5bec4c99aa3c011
Prominent <119831389+Kingmarion1@users.noreply.github.com>
2026-03-14 22:31:02 +0100

    Create authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -8,4 +8,10 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/profile", protect, profile);
 
+console.log("DEBUG - Register:", typeof register);
+console.log("DEBUG - Login:", typeof login);
+console.log("DEBUG - Profile:", typeof profile);
+console.log("DEBUG - Protect:", typeof protect);
+
+
 module.exports = router;
