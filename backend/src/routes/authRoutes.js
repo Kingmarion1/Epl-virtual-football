b7408cfad3e4ed5e4b52196ecbbfaa23a0e9bc5d
Prominent <119831389+Kingmarion1@users.noreply.github.com>
2026-03-14 22:48:09 +0100

    Updated authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { register, login, profile } = require("../controllers/authController");
-const protect = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
