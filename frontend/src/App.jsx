2b2096ea887e2dab988d03c3ab8ee0f1c0234ad6
Prominent <119831389+Kingmarion1@users.noreply.github.com>
2026-03-15 16:12:52 +0100

    Create App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Matches from "./pages/Matches";
 import Standings from "./pages/Standings";
 import Leaderboard from "./pages/Leaderboard";
 import Profile from "./pages/Profile";
-
 import Navbar from "./components/Navbar";
 import BalanceBar from "./components/BalanceBar";
-
 import BetSlip from "./components/BetSlip";
 
 function App() {
-
   return (
-
     <BrowserRouter>
-      
-      <Navbar />
-      <BalanceBar />
-
-      <Routes>
-
-        <Route path="/" element={<Matches />} />
-        <Route path="/matches" element={<Matches />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        <Route path="/table" element={<Standings />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-        <Route path="/profile" element={<Profile />} />
-
-      </Routes>
-      
-      <BetSlip />
-
+      <div className="app">
+        <Navbar />
+        <BalanceBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Matches />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/standings" element={<Standings />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </main>
+        <BetSlip />
+      </div>
     </BrowserRouter>
-
   );
-
 }
 
 export default App;
